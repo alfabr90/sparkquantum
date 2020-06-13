6d8b42f851965261a1020c28514e8566d3452168
alfabr90 <alfabr90@gmail.com>
2020-06-12 22:11:58 -0300

    Update example files

--- examples/dtqw_1d2p_broken_links.py
+++ examples/dtqw_1d2p_broken_links.py
@@ -66,31 +66,31 @@ if not entangled:
     amplitudes = []
 
     # |i>|x> --> (|0>|x> - i|1>|x>) / sqrt(2)
-    # amplitudes.append([(1.0 + 0.0j) / math.sqrt(2),
-    #                    (0.0 - 1.0j) / math.sqrt(2)])
-
-    # |i>|x> --> |0>|x>
-    amplitudes.append([(1.0 + 0.0j), 0])
-
-    # |i>|x> --> |1>|x>
-    # amplitudes.append([0, (1.0 + 0.0j)])
-
-    # |i>|x> --> (|0>|x> - i|1>|x>) / sqrt(2)
-    # amplitudes.append([(1.0 + 0.0j) / math.sqrt(2),
-    #                    (0.0 - 1.0j) / math.sqrt(2)])
+    amplitudes.append([(1.0 + 0.0j) / math.sqrt(2),
+                       (0.0 - 1.0j) / math.sqrt(2)])
 
     # |i>|x> --> |0>|x>
     # amplitudes.append([(1.0 + 0.0j), 0])
 
     # |i>|x> --> |1>|x>
-    amplitudes.append([0, (1.0 + 0.0j)])
+    # amplitudes.append([0, (1.0 + 0.0j)])
+
+    # |i>|x> --> (|0>|x> - i|1>|x>) / sqrt(2)
+    amplitudes.append([(1.0 + 0.0j) / math.sqrt(2),
+                       (0.0 - 1.0j) / math.sqrt(2)])
+
+    # |i>|x> --> |0>|x>
+    # amplitudes.append([(1.0 + 0.0j), 0])
+
+    # |i>|x> --> |1>|x>
+    # amplitudes.append([0, (1.0 + 0.0j)])
 
     initial_state = State.create(
         coin,
         mesh,
         positions,
         amplitudes,
-        interaction,
+        interaction=interaction,
         representationFormat=representationFormat)
 else:
     # Center of the mesh
@@ -98,20 +98,20 @@ else:
 
     if representationFormat == Utils.StateRepresentationFormatCoinPosition:
         # |i1>|x1>|i2>|x2> --> (|1>|x1>|0>|x2> - |0>|x1>|1>|x2>) / sqrt(2)
-        state = [[(1 * mesh_size + position) * coin_size * mesh_size + 0 * mesh_size + position, 1.0 / math.sqrt(2)],
-                 [(0 * mesh_size + position) * coin_size * mesh_size + 1 * mesh_size + position, -1.0 / math.sqrt(2)]]
+        state = [[(1 * mesh_size + position) * coin_size * mesh_size + 0 * mesh_size + position, 1, 1.0 / math.sqrt(2)],
+                 [(0 * mesh_size + position) * coin_size * mesh_size + 1 * mesh_size + position, 1, -1.0 / math.sqrt(2)]]
 
         # |i1>|x1>|i2>|x2> --> (|1>|x1>|0>|x2> + |0>|x1>|1>|x2>) / sqrt(2)
-        # state = [[(1 * mesh_size + position) * coin_size * mesh_size + 0 * mesh_size + position, 1.0 / math.sqrt(2)],
-        #          [(0 * mesh_size + position) * coin_size * mesh_size + 1 * mesh_size + position, 1.0 / math.sqrt(2)]]
+        # state = [[(1 * mesh_size + position) * coin_size * mesh_size + 0 * mesh_size + position, 1, 1.0 / math.sqrt(2)],
+        #          [(0 * mesh_size + position) * coin_size * mesh_size + 1 * mesh_size + position, 1, 1.0 / math.sqrt(2)]]
     elif representationFormat == Utils.StateRepresentationFormatPositionCoin:
         # |x1>|i1>|x2>|i2> --> (|x1>|1>|x2>|0> - |x1>|0>|x2>|1>) / sqrt(2)
-        state = [[(position * coin_size + 1) * mesh_size * coin_size + position * coin_size + 0, 1.0 / math.sqrt(2)],
-                 [(position * coin_size + 0) * mesh_size * coin_size + position * coin_size + 1, -1.0 / math.sqrt(2)]]
+        state = [[(position * coin_size + 1) * mesh_size * coin_size + position * coin_size + 0, 1, 1.0 / math.sqrt(2)],
+                 [(position * coin_size + 0) * mesh_size * coin_size + position * coin_size + 1, 1, -1.0 / math.sqrt(2)]]
 
         # |x1>|i1>|x2>|i2> --> (|x1>|1>|x2>|0> + |x1>|0>|x2>|1>) / sqrt(2)
-        # state = [[(position * coin_size + 1) * mesh_size * coin_size + position * coin_size + 0, 1.0 / math.sqrt(2)],
-        #          [(position * coin_size + 0) * mesh_size * coin_size + position * coin_size + 1, 1.0 / math.sqrt(2)]]
+        # state = [[(position * coin_size + 1) * mesh_size * coin_size + position * coin_size + 0, 1, 1.0 / math.sqrt(2)],
+        #          [(position * coin_size + 0) * mesh_size * coin_size + position * coin_size + 1, 1, 1.0 / math.sqrt(2)]]
 
     rdd = sparkContext.parallelize(state)
     shape = [(coin_size * mesh_size) ** num_particles, 1]
@@ -121,7 +121,7 @@ else:
         coin,
         mesh,
         num_particles,
-        interaction)
+        interaction=interaction)
 
 # Instantiating the walk
 dtqw = DiscreteTimeQuantumWalk(initial_state)
