41b060a07c824526b148da5a5a6bf6cc583a625e
alfabr90 <alfabr90@gmail.com>
2020-07-19 17:56:13 -0300

    Rename gauge and interaction files

--- examples/dtqw_1d1p_broken_links.py
+++ examples/dtqw_1d1p_broken_links.py
@@ -5,7 +5,7 @@ from pyspark import SparkContext, SparkConf
 from sparkquantum import constants, plot, util
 from sparkquantum.dtqw.coin.coin1d.hadamard import Hadamard
 from sparkquantum.dtqw.dtqw import DiscreteTimeQuantumWalk
-from sparkquantum.dtqw.gauge.position_gauge import PositionGauge
+from sparkquantum.dtqw.gauge.position import PositionGauge
 from sparkquantum.dtqw.mesh.broken_links.random_broken_links import RandomBrokenLinks
 from sparkquantum.dtqw.mesh.mesh1d.line import Line
 from sparkquantum.dtqw.state import State
