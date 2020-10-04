5ace75d210607091adf8394a99a0c6acf5f7c99f
alfabr90 <alfabr90@gmail.com>
2020-10-04 15:41:45 -0300

    Change coin constructor parameters
    This will allow the Grover coin to have sizes different than powers of
    two

--- sparkquantum/dtqw/coin/grover.py
+++ sparkquantum/dtqw/coin/grover.py
@@ -6,27 +6,24 @@ __all__ = ['Grover']
 class Grover(Coin):
     """Class that represents the Grover coin."""
 
-    def __init__(self, ndim):
+    def __init__(self, size):
         """Build a Grover coin object.
 
         Parameters
         ----------
-        ndim : int
-            The number of dimensions for the coin. Must be greater than 1.
+        size : int
+            The size of the quantum state to build the coin. Must be greater than 1.
 
         """
-        if ndim < 2:
+        if size < 2:
             raise ValueError("invalid number of dimensions")
 
         super().__init__()
 
-        self._ndim = ndim
-
-        n = 2 ** ndim
-        v = 2 / n
+        v = 2 / size
 
         self._data = tuple([tuple([v - (1 if i == j else 0)
-                                   for i in range(n)]) for j in range(n)])
+                                   for i in range(size)]) for j in range(size)])
 
     def __str__(self):
-        return '{}d grover coin'.format(self._ndim)
+        return 'Grover coin'
